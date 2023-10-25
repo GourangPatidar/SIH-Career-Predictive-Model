37375c2b7807dc4fa8ae421f7b2224d98dab3495
hp <gourangpatidar2003@gmail.com>
2023-10-25 16:01:13 +0530

    gpt intigrated

---
+++ chatgpt.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import openai
+
+app = Flask(__name__)
+
+# Replace 'YOUR_API_KEY' with your actual OpenAI API key
+api_key = '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'
+
+# Initialize the OpenAI API client
+openai.api_key = api_key
+
+# Send a message to ChatGPT
+
+
+def send_message(message, model="gpt-3.5-turbo"):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message},
+        ]
+    )
+
+    return response
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    try:
+        data = request.get_json()
+        user_message = data.get('message')
+
+        if not user_message:
+            return jsonify({"error": "Please provide a 'message' field in the request."})
+
+        response = send_message(user_message)
+        return jsonify({"assistant_response": response['choices'][0]['message']['content']})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
