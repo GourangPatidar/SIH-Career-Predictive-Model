d3c6e1dabf1685153391cade59d228e0c3bba690
Gourang <gourangpatidar2003@gmail.com>
2024-02-22 14:54:45 +0530

    updated

--- app.py
+++ app.py
@@ -1,64 +1,14 @@
-import pandas as pd 
-from flask import Flask, jsonify , request , render_template
-import pickle
+from flask import Flask, request, render_template
 from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
-import openai
 
-# Replace 'YOUR_API_KEY' with your actual OpenAI API key
-api_key = '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'
+application = Flask(__name__)
 
-# Initialize the OpenAI API client
-openai.api_key = api_key
-
-# Send a message to ChatGPT
-
-
-def send_message(message, model="gpt-3.5-turbo"):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message},
-        ]
-    )
-
-    return response
-
-
-
-
-app=Flask(__name__)
+app = application
 
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
-
-
-
-
-@app.route("/predict" , methods=['POST'])
-def predict():
-    json=request.json
-    query_df=pd.DataFrame(json)
-    predict_pipeline = PredictPipeline()
-    prediction = predict_pipeline.predict(query_df)
-    return jsonify({"prediction" :list(prediction)})
-
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    try:
-        data = request.get_json()
-        user_message = data.get('message')
-
-        if not user_message:
-            return jsonify({"error": "Please provide a 'message' field in the request."})
-
-        response = send_message(user_message)
-        return jsonify({"assistant_response": response['choices'][0]['message']['content']})
-    except Exception as e:
-        return jsonify({"error": str(e)})
 
 
 @app.route('/predict', methods=['GET', 'POST'])
@@ -75,12 +25,14 @@ def predict_datapoint():
             personality=request.form.get('personality'),
 
         )
-        json = request.json
-        query_df = pd.DataFrame(json)
+        final_new_data = data.get_data_as_dataframe()
         predict_pipeline = PredictPipeline()
-        prediction = predict_pipeline.predict(query_df)
-        return jsonify({"prediction": list(prediction)})
-        
+        pred = predict_pipeline.predict(final_new_data)
 
-if __name__=="__main__":
-    app.run(debug=True)
+        results = pred[0]
+
+        return render_template('results.html', final_result=results)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
